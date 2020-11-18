ae20b393d977ae83cf05f51ae03f98b8b9bd6317
yuangli <yuangli@localhost>
2020-11-18 14:58:38 -0500

    FileReaderDone

--- Compression/src/Compressor.java
+++ Compression/src/Compressor.java
@@ -1,5 +1,38 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Compressor {
 
+	Map<Character, Integer> FrequencyMap;
 	
+	public void CalculateFrequency(String path){
+		FrequencyMap = new HashMap<Character, Integer>(); 
+		try {
+			FileReader reader = new FileReader(path);
+			int c;
+			while((c = reader.read()) != -1){
+				if (FrequencyMap.containsKey((char)c)){
+					FrequencyMap.put((char)c, FrequencyMap.get((char)c)+1);
+				}else{
+					FrequencyMap.put((char)c, 1);
+				}
+			}
+		} catch (IOException e) {
+			System.out.print("file not found");
+			e.printStackTrace();
+		}
+	}
+	
+	public void PrintFrequencyMao(){
+		System.out.println(FrequencyMap.entrySet());
+	}
+	
+	public static void main(String[] args){
+		Compressor comp = new Compressor();
+		comp.CalculateFrequency("testfile");
+		comp.PrintFrequencyMao();
+	}
 }
